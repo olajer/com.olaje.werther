751bf14e31b4049066109fe10914ecad2dde5fac
Raul Olaje <olajer@hotmail.com>
2017-11-23 22:57:38 -0700

    Version final

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,166 +1,128 @@
 angular.module('app', ['ionic'])
 
 .config(function($stateProvider, $urlRouterProvider) {
-
   $stateProvider
-    .state('tabs', {
-      url: "/tab",
-      abstract: true,
-      templateUrl: "js/templates/tabs.html"
-    })
-    .state('tabs.home', {
-      url: "/home",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/home.html",
-          controller: 'HomeTabCtrl'
-        }
-      }
-    })
-    .state('tabs.pregunta_a', {
-      url: "/pregunta_a",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_a.html"
-        }
-      }
-    })
-    .state('tabs.pregunta_a_si', {
-      url: "/pregunta_a_si",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_a_si.html"
-        }
-      }
-    })
-      .state('tabs.pregunta_a_no', {
-      url: "/pregunta_a_no",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_a_no.html"
-        }
-      }
-    })
-    .state('tabs.pregunta_b', {
-      url: "/pregunta_b",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_b.html"
-        }
-      }
-    })
-    .state('tabs.pregunta_b_si', {
-      url: "/pregunta_b_si",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_b_si.html"
-        }
-      }
-    })
-      .state('tabs.pregunta_b_no', {
-      url: "/pregunta_b_no",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_b_no.html"
-        }
-      }
-    }) 
-    .state('tabs.pregunta_c', {
-      url: "/pregunta_c",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_c.html"
-        }
-      }
-    })
-    .state('tabs.pregunta_c_si', {
-      url: "/pregunta_c_si",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_c_si.html"
-        }
-      }
-    })
-      .state('tabs.pregunta_c_no', {
-      url: "/pregunta_c_no",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_c_no.html"
-        }
-      }
-    })
-    .state('tabs.pregunta_d', {
-      url: "/pregunta_d",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_d.html"
-        }
-      }
-    })
-    .state('tabs.pregunta_d_si', {
-      url: "/pregunta_d_si",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_d_si.html"
-        }
-      }
-    })
-      .state('tabs.pregunta_d_no', {
-      url: "/pregunta_d_no",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_d_no.html"
-        }
-      }
-    })
-    .state('tabs.pregunta_e', {
-      url: "/pregunta_e",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_e.html"
-        }
-      }
-    })
-    .state('tabs.pregunta_e_si', {
-      url: "/pregunta_e_si",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_e_si.html"
-        }
-      }
-    })
-      .state('tabs.pregunta_e_no', {
-      url: "/pregunta_e_no",
-      views: {
-        'home-tab': {
-          templateUrl: "js/templates/pregunta_e_no.html"
-        }
-      }
-    })                     
-    .state('tabs.about', {
-      url: "/about",
-      views: {
-        'about-tab': {
-          templateUrl: "js/templates/about.html"
-        }
-      }
-    })
-    .state('tabs.contact', {
-      url: "/contact",
-      views: {
-        'contact-tab': {
-          templateUrl: "js/templates/contact.html"
-        }
-      }
-    });
-   $urlRouterProvider.otherwise("/tab/home");
+    .state('home', {url: "/home",templateUrl: "js/templates/home.html"})
+    .state('inicio', {url: "/inicio",templateUrl: "js/templates/inicio.html"})    
+    .state('pregunta_a', {url: "/pregunta_a",templateUrl: "js/templates/pregunta_a.html"})
+    .state('pregunta_a_si', {url: "/pregunta_a_si",templateUrl: "js/templates/pregunta_a_si.html"})
+    .state('pregunta_a_no', {url: "/pregunta_a_no",templateUrl: "js/templates/pregunta_a_no.html"})
+    .state('pregunta_b', {url: "/pregunta_b",templateUrl: "js/templates/pregunta_b.html"})
+    .state('pregunta_b_si', {url: "/pregunta_b_si",templateUrl: "js/templates/pregunta_b_si.html"})
+    .state('pregunta_b_no', {url: "/pregunta_b_no",templateUrl: "js/templates/pregunta_b_no.html"}) 
+    .state('pregunta_c', {url: "/pregunta_c",templateUrl: "js/templates/pregunta_c.html"})
+    .state('pregunta_c_si', {url: "/pregunta_c_si",templateUrl: "js/templates/pregunta_c_si.html"})
+    .state('pregunta_d', {url: "/pregunta_d",templateUrl: "js/templates/pregunta_d.html"})
+    .state('pregunta_d_si', {url: "/pregunta_d_si",templateUrl: "js/templates/pregunta_d_si.html"})
+    .state('pregunta_e', {url: "/pregunta_e",templateUrl: "js/templates/pregunta_e.html"})
+    .state('pregunta_e_si', {url: "/pregunta_e_si",templateUrl: "js/templates/pregunta_e_si.html"})
+    .state('escalas', {url: "/escalas",templateUrl: "js/templates/escalas.html"})
+    .state('escalas_a', {url: "/escalas_a",templateUrl: "js/templates/escalas_a.html"})
+    .state('escalas_b', {url: "/escalas_b",templateUrl: "js/templates/escalas_b.html"})
+    .state('escalas_c', {url: "/escalas_c",templateUrl: "js/templates/escalas_c.html"})
+    .state('escalas_d', {url: "/escalas_d",templateUrl: "js/templates/escalas_d.html"})
+    .state('manejo', {url: "/manejo",templateUrl: "js/templates/manejo.html"})
+    .state('manejo_a', {url: "/manejo_a",templateUrl: "js/templates/manejo_a.html"}) 
+    .state('manejo_b', {url: "/manejo_b",templateUrl: "js/templates/manejo_b.html"}) 
+    .state('manejo_c', {url: "/manejo_c",templateUrl: "js/templates/manejo_c.html"}) 
+    .state('manejo_d', {url: "/manejo_d",templateUrl: "js/templates/manejo_d.html"}) 
+    .state('manejo_e', {url: "/manejo_e",templateUrl: "js/templates/manejo_e.html"})                     
+    .state('about', {url: "/about",templateUrl: "js/templates/about.html"})
+    .state('contact', {url: "/contact",templateUrl: "js/templates/contact.html"});
+   $urlRouterProvider.otherwise("/home");
 
 })
+.controller('EscalasCtrl', function($scope) {
+    $scope.total = 0;
 
-.controller('HomeTabCtrl', function($scope) {
-  $scope.lol = "foo";
-  
-  $scope.baz = function() {
-    $scope.lol = "bar";
-  }
+    $scope.nino={
+        a:{seleccionado:false, valor:1},
+        b:{seleccionado:false, valor:1},
+        c:{seleccionado:false, valor:2},
+        d:{seleccionado:false, valor:1},
+        e:{seleccionado:false, valor:3},
+        f:{seleccionado:false, valor:3},
+        g:{seleccionado:false, valor:2},
+        h:{seleccionado:false, valor:2},
+        i:{seleccionado:false, valor:3},
+        j:{seleccionado:false, valor:2}
+    };
+   
+    $scope.getTotalNinos = function(){
+      $scope.total = 0;
+        for(index in $scope.nino){
+          if($scope.nino[index].seleccionado){
+            var valor=$scope.nino[index].valor;
+            $scope.total += valor;
+          }
+        }
+    }
+
+   $scope.adolescente={
+        a:{seleccionado:false, valor:1},
+        b:{seleccionado:false, valor:2},
+        c:{seleccionado:false, valor:3},
+        d:{seleccionado:false, valor:2},
+        e:{seleccionado:false, valor:4},
+        f:{seleccionado:false, valor:5},
+        g:{seleccionado:false, valor:2},
+        h:{seleccionado:false, valor:5},
+        i:{seleccionado:false, valor:4},
+        j:{seleccionado:false, valor:2}
+    };
+   
+    $scope.getTotalAdolescentes = function(){
+      $scope.total = 0;
+        for(index in $scope.adolescente){
+          if($scope.adolescente[index].seleccionado){
+            var valor=$scope.adolescente[index].valor;
+            $scope.total += valor;
+          }
+        }
+    }
+
+   $scope.adulto={
+        a:{seleccionado:false, valor:3},
+        b:{seleccionado:false, valor:3},
+        c:{seleccionado:false, valor:2},
+        d:{seleccionado:false, valor:4},
+        e:{seleccionado:false, valor:5},
+        f:{seleccionado:false, valor:4},
+        g:{seleccionado:false, valor:2},
+        h:{seleccionado:false, valor:3},
+        i:{seleccionado:false, valor:3},
+        j:{seleccionado:false, valor:2}
+    };
+   
+    $scope.getTotalAdultos = function(){
+      $scope.total = 0;
+        for(index in $scope.adulto){
+          if($scope.adulto[index].seleccionado){
+            var valor=$scope.adulto[index].valor;
+            $scope.total += valor;
+          }
+        }
+    }    
+   $scope.pmayor={
+        a:{seleccionado:false, valor:2},
+        b:{seleccionado:false, valor:2},
+        c:{seleccionado:false, valor:3},
+        d:{seleccionado:false, valor:3},
+        e:{seleccionado:false, valor:1},
+        f:{seleccionado:false, valor:2},
+        g:{seleccionado:false, valor:4},
+        h:{seleccionado:false, valor:5},
+        i:{seleccionado:false, valor:3},
+        j:{seleccionado:false, valor:5}
+    };
+   
+    $scope.getTotalMayor = function(){
+      $scope.total = 0;
+        for(index in $scope.pmayor){
+          if($scope.pmayor[index].seleccionado){
+            var valor=$scope.pmayor[index].valor;
+            $scope.total += valor;
+          }
+        }
+    }
 });
